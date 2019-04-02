6f44aff0cf6c50b523a29a37ad475d4ef5632bf9
Richard A. Johansen <richard.johansen@uc.edu>
2019-04-02 12:03:36 -0400

    Add files via upload

---
+++ install.R
@@ -0,0 +1,7 @@
+install.packages('ggplot2')
+install.packages("tmap")
+<<<<<<< HEAD
+install.packages('tidyverse')
+=======
+install.packages('tidyverse')
+>>>>>>> 2856e9ffef45636165f9c2962ae950de01adbc56
