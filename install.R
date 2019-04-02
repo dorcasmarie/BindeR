5306775028c5e7e29f17895877c8e14bc00aa92b
Richard A. Johansen <richard.johansen@uc.edu>
2019-04-02 12:04:37 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,7 +1,1 @@
 install.packages('ggplot2')
-install.packages("tmap")
-<<<<<<< HEAD
-install.packages('tidyverse')
-=======
-install.packages('tidyverse')
->>>>>>> 2856e9ffef45636165f9c2962ae950de01adbc56
