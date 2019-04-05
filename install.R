6e27cef54391c2da2fb02192d2cc02d7391cd4e4
Richard A. Johansen <richard.johansen@uc.edu>
2019-04-05 10:50:15 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,2 @@
 install.packages("tidyverse")
-install.packages("rgeos", repos='http://cran.us.r-project.org')
-install.packages("rgdal", repos='http://cran.us.r-project.org')	
-install.packages("tmap", repos='http://cran.us.r-project.org')	
+install.packages("tmap")
